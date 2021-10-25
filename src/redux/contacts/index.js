4c14e1161435fb5dddf410a7023cc0ea6bc93b9d
Wences81 <zhylinpochta@gmail.com>
2021-10-25 19:54:43 +0200

    start2

--- src/redux/contacts/index.js
+++
@@ -1,4 +0,0 @@
-export * as contactsActions from "./contacts-actions";
-export * as getVisibleContacts from "./contacts-selectors";
-export * as contactsSelectors from "./contacts-selectors";
-export * as contactsOperations from "./contacts-operations";
---
+++ src/redux/phonebook/index.js
@@ -0,0 +1,4 @@
+export * as contactsActions from "./contacts-actions";
+export * as getVisibleContacts from "./phonebook-selectors";
+export * as contactsSelectors from "./phonebook-selectors";
+export * as contactsOperations from "./contacts-operations";
