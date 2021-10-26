cc7e95611e16ed9024c362d322adaa990606d115
Wences81 <zhylinpochta@gmail.com>
2021-10-26 20:29:38 +0200

    day2.3

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = "https://connections-api.herokuapp.com";
+
+export const token = {
+  set(token) {
+    axios.defaults.headers.common.Autorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Autorization = "";
+  },
+};
+
+export const logIn = createAsyncThunk(
+  "auth/login",
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("/users/login", credentials);
+      token.set(data.token);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.data);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk(
+  "auth/logout",
+  async (_, { rejectWithValue }) => {
+    try {
+      await axios.post("/users/logout");
+      token.unset();
+    } catch (error) {
+      return rejectWithValue(error.data);
+    }
+  }
+);
+
+export const fetchCurrentUser = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get("/users/current");
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  }
+);
